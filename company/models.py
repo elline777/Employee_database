e7b3b9da8e96ce30e5968b3006285523292916ad
Natalia <47737431+elline777@users.noreply.github.com>
2024-09-13 14:32:46 +0500

    Create Employee, Department, Branch models

---
+++ company/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    department = models.ForeignKey(
+        'company.Department',
+        on_delete=models.CASCADE,
+        related_name='employees',
+        null=True
+    )
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    store = models.IntegerField(unique=True)
+    branch = models.ForeignKey(
+        'company.Branch',
+        on_delete=models.SET_NULL,
+        related_name='departments',
+        null=True
+    )
+
+
+class Branch(models.Model):
+    address = models.CharField(max_length=255, blank=True, null=True)
+    short_name = models.CharField(max_length=100)
+
+
+
