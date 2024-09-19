ec4f796e9d0b3ec29ee5b439d6d5123e8fc4d8f3
Natalia <47737431+elline777@users.noreply.github.com>
2024-09-19 20:19:03 +0500

    Add main page test

--- company/tests.py
+++ company/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class GetPagesTestCase(TestCase):
+    def setUp(self):
+        "Инициализация перед выполнением каждого теста"
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def tearDown(self):
+        "Действие после выполнения каждого теста"
+
