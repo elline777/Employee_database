5ef246fe51e431b411854c9478f84838533b7e6c
Natalia <47737431+elline777@users.noreply.github.com>
2024-09-20 21:13:33 +0500

    Add pylint disables

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # pylint: disable=import-outside-toplevel
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm.settings')
     try:
         from django.core.management import execute_from_command_line
